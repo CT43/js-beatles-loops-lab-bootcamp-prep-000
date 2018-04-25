77edd7be98ee73a9efe36019515c78d19145ca50
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-04-25 20:13:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function theBeatlesPlay() {
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians,instruments) {
   
 }
